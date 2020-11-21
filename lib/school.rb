f8d898b1c6b62fec9d90a52e7e5116aab70ac35f
Meredith <meredith@mcintosh.io>
2020-11-21 15:14:23 -0500

    commit tree 9ef3142c9834002f96ece43afb2757bf07803a8a

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+require "pry"
+class School
+    
+    attr_accessor :roster, :add_student
+
+    def initialize(roster)
+        @roster = {}
+        
+    end
+
+    def add_student(name, grade)
+        @roster[grade] || = []
+        roster[grade] << name
+       
+    end
+# binding.pry
+end
